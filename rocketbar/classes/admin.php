5369b01fd5893212e03a3d9afe23c64d5db4853c
Bruce <bruce@myinbox.ws>
2015-05-13 20:14:34 -0400

    Adding fuzzy matching lib

--- rocketbar/classes/admin.php
+++ rocketbar/classes/admin.php
@@ -10,6 +10,7 @@ class admin {
 	public static function get_all_pages() {
 		global $menu, $submenu;
 
-		global $_wp_real_parent_file, $_wp_submenu_nopriv, $_registered_pages, $_parent_pages;
+		// TODO save the menu and submenu globals here, in a way that can be referenced later.
+		//var_dump($menu);
 	}
 }
