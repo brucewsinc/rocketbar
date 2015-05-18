37084ed6642cf683f30a23540472cdca3202b003
Bruce <bruce@myinbox.ws>
2015-05-18 14:06:22 -0400

    Reformat and version up

--- rocketbar/rocketbar.php
+++ rocketbar/rocketbar.php
@@ -4,7 +4,7 @@
  * Author: Code by Jinx
  * Author URI: http://byjinx.com/
  * Description: The quicker page switcher!? Keyboard shortcuts for your WordPress Dashboard.
- * Version: 000000-dev
+ * Version: 150518
  */
 
 /**
@@ -25,6 +25,6 @@
  */
 
 $GLOBALS['wp_php_rv'] = '5.3';
-if(require(dirname(__FILE__).'/includes/wp-php-rv/check.php'))
-	require dirname(__FILE__).'/includes/rocketbar.inc.php';
+if(require(dirname(__FILE__) . '/includes/wp-php-rv/check.php'))
+	require dirname(__FILE__) . '/includes/rocketbar.inc.php';
 else wp_php_rv_notice();
