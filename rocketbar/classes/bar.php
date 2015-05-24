e6f20f368ab178f789bb930824145ed52bda3e88
Bruce <bruce@myinbox.ws>
2015-05-24 09:24:27 -0400

    Updating commands JS

--- rocketbar/classes/bar.php
+++ rocketbar/classes/bar.php
@@ -11,7 +11,7 @@ class bar {
 
 		wp_enqueue_script('fuzzy-matching', plugin()->url . '/client-s/js/fuzzy-matching.min.js', TRUE);
 		wp_enqueue_script('rocketbar-cache', site_url('/?_rocketbar_cache=1'), TRUE);
-		wp_enqueue_script('rocketbar', plugin()->url . '/client-s/js/rocketbar.min.js', array('jquery'), TRUE);
+		wp_enqueue_script('rocketbar', plugin()->url . '/client-s/js/rocketbar.js', array('jquery'), TRUE);
 
 		wp_enqueue_style('dashicons');
 		wp_enqueue_style('rocketbar', plugin()->url . '/client-s/css/rocketbar.min.css');
